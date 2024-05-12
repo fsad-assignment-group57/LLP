58438ee233df34fbc23ed8e5f3d7f02c07c45d47
sampadap03 <sampadap03@gmail.com>
2024-05-12 15:33:12 +0530

    Add implementation of user streak

--- routes/userStreak.py
+++ routes/userStreak.py
@@ -1,6 +1,7 @@
 from flask import request
 from flask_restful import Resource
 from flask_restful_swagger import swagger
+from datetime import datetime, timedelta
 
 from dbAPI import mysql
 
@@ -10,7 +11,7 @@ class userStreak(Resource):
         self.__DB_table = "user_streak"
 
     @swagger.operation(
-        notes='Update user streak for logging in',
+        notes='Update user streak',
         parameters=[
             {
                 "name": "user_id",
@@ -24,41 +25,56 @@ class userStreak(Resource):
         nickname='update_streak'
     )
     def post(self, user_id):
-        data = request.get_json()
-        user_id = data.get('user_id')
-
-        cursor = mysql.connection.cursor()
-        cursor.execute("SELECT language FROM user_languages WHERE user_id = %s", (user_id))
-        languages = [row[0] for row in cursor.fetchall()]
-
-        cursor.close()
-
-        if not languages:
-            return {'error': 'User ID not found'}, 404
-
-        if user_id is None or languages is None:
-            return {'error': 'User ID or languages not provided'}, 400
-        
-        cursor = mysql.connection.cursor()
         try:
-            for language in languages:
-                cursor.execute(f"""INSERT INTO {self.__DB_table} (user_id, language) 
-                    SELECT * FROM (SELECT {user_id}, '{language}') AS temp
-                    WHERE NOT EXISTS ( SELECT 1
-                    FROM {self.__DB_table}
-                    WHERE user_id = {user_id} AND language = '{language}')""")
-                
-                mysql.connection.commit()
+            cursor = mysql.connection.cursor()
+            cursor.execute(f"SELECT streak, update_time FROM {self.__DB_table} WHERE user_id = '{user_id}'")
 
+            result = cursor.fetchone()
+            if result:
+                latest_streak, latest_update_time = result
+            else:
+                latest_streak, latest_update_time = 0, None
+            
+            current_time = datetime.now()
+            message = ''
+
+            if latest_update_time:
+                day_before_yesterday_end = datetime.now().replace(hour=23, minute=59, second=59, microsecond=59) - timedelta(days=2)
+
+                # Check if the update_time is anytime of yesterday
+                if latest_update_time.date() == (current_time - timedelta(days=1)).date():
+                    latest_streak += 1
+                    print(f"User {user_id}'s streak has been updated. Current streak: {latest_streak}")
+                    message = f"User {user_id}'s streak has been updated. Current streak: {latest_streak}"
+
+                    cursor.execute(f"UPDATE {self.__DB_table} SET streak = {latest_streak}, update_time = '{current_time.strftime('%Y-%m-%d %H:%M:%S')}' WHERE user_id = '{user_id}'")
+                elif latest_update_time < day_before_yesterday_end:
+                    latest_streak = 1
+                    print(f"User {user_id}'s streak has been reset for a new day.")
+                    message = f"User {user_id}'s streak has been reset for a new day"
+                
+                    cursor.execute(f"UPDATE {self.__DB_table} SET streak = {latest_streak}, update_time = '{current_time.strftime('%Y-%m-%d %H:%M:%S')}' WHERE user_id = '{user_id}'")
+                else:
+                    print(f"User {user_id} has updated their streak recently.")
+                    message = f"User {user_id} has updated their streak recently"
+
+            else:
+                # No previous update found, start a new streak
+                latest_streak = 1
+                print(f"Starting a new streak for user {user_id}.")
+                message = f"User {user_id}'s streak has been reset for a new day"
+
+                cursor.execute(f"INSERT INTO {self.__DB_table} (user_id, streak, update_time) VALUES ('{user_id}', {latest_streak}, '{current_time.strftime('%Y-%m-%d %H:%M:%S')}')")
+            
+            mysql.connection.commit()
             cursor.close()
-            return {'message': 'Registered languages stored successfully'}
-        
+            return {'message': message}
         except Exception as e:
             cursor.close()
             return {'error': str(e)}, 500
         
     @swagger.operation(
-        notes='Fetch registered languages for a user',
+        notes='Fetch user',
         parameters=[
             {
                 "name": "user_id",
@@ -69,18 +85,20 @@ class userStreak(Resource):
             }
         ],
         responseClass=dict,
-        nickname='fetch_languages'
+        nickname='fetch_streak'
     )
     def get(self, user_id):
-        cursor = mysql.connection.cursor()
-        cursor.execute("SELECT language FROM user_languages WHERE user_id = %s", (user_id))
-        languages = [row[0] for row in cursor.fetchall()]
+        try:
+            cursor = mysql.connection.cursor()
+            cursor.execute(f"SELECT streak FROM {self.__DB_table} WHERE user_id = '{user_id}'")
+            streak = cursor.fetchone()
 
-        cursor.close()
+            cursor.close()
 
-        if not languages:
-            return {'error': 'User ID not found'}, 404
-        
-        return {'user_id': user_id, 'languages': languages}
-        
-    
+            if not streak:
+                return {'error': 'User ID not found'}, 404
+            
+            return {'user_id': user_id, 'streak': int(streak[0])}
+        except Exception as e:
+            cursor.close()
+            return {'error': str(e)}, 500
--- server.py
+++ server.py
@@ -18,12 +18,15 @@ setMySQLConfig(app)
 from routes.hello_world import HelloWorld
 from routes.square import Square
 from routes.userLanguages import userLanguages
+from routes.userStreak import userStreak
 
 # Add routes
 api.add_resource(HelloWorld, '/')
 api.add_resource(Square, '/square')
 api.add_resource(userLanguages, '/user-languages/', endpoint='user_languages_post')
 api.add_resource(userLanguages, '/user-languages/<string:user_id>',  endpoint='user_languages_get')
+
+api.add_resource(userStreak, '/user-streak/<string:user_id>',  endpoint='user_streak')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
